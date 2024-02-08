4aca0184259bc2fe20e05563e953f600018e15dd
ManyManyCrystals <kuznetsov.vladislav@internet.ru>
2024-02-09 02:02:23 +0500

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,34 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
+};
+
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
+};
+
+Student.prototype.addMarks = function(...marksToAdd) {
+	let marks = this.marks;
+	if (marks !== undefined) {
+		marks.push(...marksToAdd);
+	};
+};
+
+Student.prototype.getAverage = function() {
+	let marks = this.marks;
+	if (marks === undefined || marks.length === 0 ) {
+    return 0
+	}
+  let sum = marks.reduce((acc, value) => acc + value, 0)
+  return sum / marks.length;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
-}
+Student.prototype.exclude = function(reason) {
 
-Student.prototype.addMarks = function (...marks) {
-  
-}
+	delete this.marks;
+	delete this.subject;
+	this.excluded = reason;
 
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
 }
